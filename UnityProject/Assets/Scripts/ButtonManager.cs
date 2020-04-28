642eb7122ce571462ffd263abdd8080e677fba38
kandabi <kandabiaviv@gmail.com>
2020-04-28 15:23:43 +0300

    Git Test

---
+++ UnityProject/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    private Button commandButton;
+    private Button takeoffButton;
+    private Button landButton;
+    private Button streamOnButton;
+    private Button streamOffButton;
+    private TelloManager manager;
+
+    void Start()
+    {
+        manager = GameObject.Find("Scripts").GetComponent<TelloManager>();
+
+        commandButton = GameObject.Find("command").GetComponent<Button>();
+        takeoffButton = GameObject.Find("takeoff").GetComponent<Button>();
+        landButton = GameObject.Find("land").GetComponent<Button>();
+        streamOnButton = GameObject.Find("streamon").GetComponent<Button>();
+
+        commandButton.onClick.AddListener(commandOnClick);
+        takeoffButton.onClick.AddListener(takeoffOnClick);
+        landButton.onClick.AddListener(landOnClick);
+        streamOnButton.onClick.AddListener(streamOnClick);
+    }
+
+    void commandOnClick()
+    {
+        manager.Command();
+    }
+
+    void takeoffOnClick()
+    {
+        manager.TakeOff();
+    }
+
+    void landOnClick()
+    {
+        manager.Land();
+    }
+
+    void streamOnClick()
+    {
+        manager.StreamOn();
+    }
+
+}
---
+++ UnityProject/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class NetworkManager 
+{
+    //Simple UDP client/server
+    public struct Received
+    {
+        public IPEndPoint Sender;
+        public string Message;
+        public byte[] bytes;
+    }
+
+    public abstract class UdpBase
+    {
+        protected UdpClient Client;
+
+        protected UdpBase()
+        {
+            Client = new UdpClient();
+        }
+
+        public async Task<Received> Receive()
+        {
+            var result = await Client.ReceiveAsync();
+            return new Received()
+            {
+                bytes = result.Buffer.ToArray(),
+                Message = Encoding.ASCII.GetString(result.Buffer, 0, result.Buffer.Length),
+                Sender = result.RemoteEndPoint
+            };
+        }
+    }
+
+    //Server
+    public class UdpListener : UdpBase
+    {
+        private IPEndPoint listenOn;
+
+        public UdpListener(int port, string address = "0.0.0.0")
+        {
+            Client = new UdpClient(new IPEndPoint(IPAddress.Parse(address), port));
+        }
+    }
+
+    //Client
+    public class UdpSender : UdpBase
+    {
+        public void ConnectTo(string hostname, int port)
+        {
+            Client.Connect(hostname, port);
+        }
+
+        public int Send(string message)
+        {
+            var datagram = Encoding.ASCII.GetBytes(message);
+            Client.Send(datagram, datagram.Length);
+            return datagram.Length;
+        }
+        public int Send(byte[] message)
+        {
+            Client.Send(message, message.Length);
+            return message.Length;
+        }
+    }
+}
